b799067dc4113debbc105b7df619f1cafd2aaca8
Ayodele Fagbami <afagbami@gmail.com>
2022-07-31 19:03:04 +0100

    added body widget for onboarding

--- lib/features/onboarding/presentation/pages/onboarding_screen.dart
+++ lib/features/onboarding/presentation/pages/onboarding_screen.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/body.dart';
+
 class OnboardingScreen extends StatelessWidget {
   static String routeName = "/onboarding";
   const OnboardingScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return const Scaffold(
+      body: Body(),
+    );
   }
 }

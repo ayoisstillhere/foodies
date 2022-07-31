1f044eefcf1f3a0ed353eddd2d7658190d43fa34
Ayodele Fagbami <afagbami@gmail.com>
2022-07-31 18:23:50 +0100

    created onboarding screen and added routeName

---
+++ lib/features/onboarding/presentation/pages/onboarding_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class OnboardingScreen extends StatelessWidget {
+  static String routeName = "/onboarding";
+  const OnboardingScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}

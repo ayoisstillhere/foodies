e02275e0f2306f2bacc396d12fb15e2731c8b87d
Ayodele Fagbami <afagbami@gmail.com>
2022-07-31 18:28:34 +0100

    moved color constants to right file

--- lib/size_config.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-
-const kPrimaryColor = Color(0xFF191919);
-const kSecondaryColor = Color(0xFFF15025);
-const kTextGreyColor = Color(0xFFCED0CE);
-const kObjectGreyColor = Color(0xFFE6E8E6);

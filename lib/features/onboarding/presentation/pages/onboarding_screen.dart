599d0924982d48ac94c41e5a0740f4df1eec53fb
Ayodele Fagbami <afagbami@gmail.com>
2022-07-31 21:03:34 +0100

    called init on sized config(important!!!)
    Has to be called on first page, otherwise it will show error

--- lib/features/onboarding/presentation/pages/onboarding_screen.dart
+++ lib/features/onboarding/presentation/pages/onboarding_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import '../../../../size_config.dart';
 import '../widgets/body.dart';
 
 class OnboardingScreen extends StatelessWidget {
@@ -8,6 +9,8 @@ class OnboardingScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // Has to Be called on Starting Screen
+    SizeConfig().init(context);
     return const Scaffold(
       body: Body(),
     );

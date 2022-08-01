2b4ecbd0c9639fbe1bff54fe49f7901e6e92bfb4
Ayodele Fagbami <afagbami@gmail.com>
2022-08-01 22:10:38 +0100

    created login screen and added to Routes

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:foodies/features/login/presentation/pages/login_screen.dart';
 
 import 'features/onboarding/presentation/pages/onboarding_screen.dart';
 
@@ -6,4 +7,5 @@ import 'features/onboarding/presentation/pages/onboarding_screen.dart';
 // all routes wull be made available here
 final Map<String, WidgetBuilder> routes = {
   OnboardingScreen.routeName: (context) => const OnboardingScreen(),
+  LoginScreen.routeName: (context) => const LoginScreen(),
 };

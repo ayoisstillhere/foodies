2dbc9377fff4f3eeea58abf26c9570886b99ad2c
Ayodele Fagbami <afagbami@gmail.com>
2022-08-03 14:09:49 +0100

    Create create_current_user_usecase.dart

---
+++ lib/features/signup/domain/usecases/create_current_user_usecase.dart
@@ -0,0 +1,22 @@
+import 'package:foodies/features/signup/domain/repositories/firebase_repository.dart';
+
+class CreateCurrentUserUsecase {
+  final FirebaseRepository repository;
+
+  CreateCurrentUserUsecase(this.repository);
+
+  Future<void> call(
+    String email,
+    String firstName,
+    String lastName,
+    String uid,
+    String userClass,
+    String hall,
+    String floor,
+    String wing,
+    String roomNo,
+  ) async {
+    repository.createCurrentUser(
+        email, firstName, lastName, uid, userClass, hall, floor, wing, roomNo);
+  }
+}

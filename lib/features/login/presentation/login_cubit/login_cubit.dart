ce697bc8d2a21e102026658c9c0438d73abee79f
Ayodele Fagbami <afagbami@gmail.com>
2022-08-03 19:21:00 +0100

    state mgmt for login setup

--- lib/features/login/presentation/login_cubit/login_cubit.dart
+++ lib/features/login/presentation/login_cubit/login_cubit.dart
@@ -1,8 +1,51 @@
+import 'dart:io';
+
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:foodies/features/signup/domain/usecases/create_current_user_usecase.dart';
+import 'package:foodies/features/signup/domain/usecases/login_usecase.dart';
+import 'package:foodies/features/signup/domain/usecases/signup_usecase.dart';
 
 part 'login_state.dart';
 
 class LoginCubit extends Cubit<LoginState> {
-  LoginCubit() : super(LoginInitial());
+  final SignupUseCase signupUseCase;
+  final LoginUseCase loginUseCase;
+  final CreateCurrentUserUsecase createCurrentUserUsecase;
+
+  LoginCubit({
+    required this.signupUseCase,
+    required this.loginUseCase,
+    required this.createCurrentUserUsecase,
+  }) : super(LoginInitial());
+
+  Future<void> submitLogin({
+    required String email,
+    required String password,
+  }) async {
+    emit(LoginLoading());
+    try {
+      loginUseCase.call(email, password);
+      emit(LoginSuccess());
+    } on SocketException catch (e) {
+      emit(LoginFailure(e.message));
+    } catch (_) {
+      emit(LoginFailure("Firebase Exception"));
+    }
+  }
+
+  Future<void> submitRegisteration({
+    required String email,
+    required String password,
+  }) async {
+    emit(LoginLoading());
+    try {
+      signupUseCase.call(email, password);
+      emit(LoginSuccess());
+    } on SocketException catch (e) {
+      emit(LoginFailure(e.message));
+    } catch (_) {
+      emit(LoginFailure("Firebase Exception"));
+    }
+  }
 }
--- lib/features/login/presentation/login_cubit/login_state.dart
+++ lib/features/login/presentation/login_cubit/login_state.dart
@@ -2,9 +2,29 @@ part of 'login_cubit.dart';
 
 abstract class LoginState extends Equatable {
   const LoginState();
+}
 
+class LoginInitial extends LoginState {
   @override
   List<Object> get props => [];
 }
 
-class LoginInitial extends LoginState {}
+class LoginLoading extends LoginState {
+  @override
+  List<Object?> get props => [];
+}
+
+class LoginFailure extends LoginState {
+  final String errorMessage;
+
+  LoginFailure(this.errorMessage);
+
+  @override
+  List<Object?> get props => [this.errorMessage];
+}
+
+class LoginSuccess extends LoginState {
+  @override
+  List<Object?> get props => [];
+
+}

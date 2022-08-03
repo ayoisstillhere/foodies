978cfa711eb0e13ff5cf1d172c2f61c2b4088cfb
Ayodele Fagbami <afagbami@gmail.com>
2022-08-03 13:14:32 +0100

    created firebase repo impl

---
+++ lib/features/signup/data/repositories/firebase_repository_impl.dart
@@ -0,0 +1,9 @@
+import 'package:foodies/features/signup/domain/repositories/firebase_repository.dart';
+
+class FirebaseRepositoryImpl implements FirebaseRepository {
+  @override
+  Future<void> signUp(String email, String password) {
+    // TODO: implement signUp
+    throw UnimplementedError();
+  }
+}

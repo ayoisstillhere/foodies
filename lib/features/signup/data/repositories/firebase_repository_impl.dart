dbf8fefb3ea9df777cd4a8a26b6749f7e1af2d14
Ayodele Fagbami <afagbami@gmail.com>
2022-08-03 14:15:05 +0100

    implemented firebase repository methods

--- lib/features/signup/data/repositories/firebase_repository_impl.dart
+++ lib/features/signup/data/repositories/firebase_repository_impl.dart
@@ -1,33 +1,47 @@
+import 'package:foodies/features/signup/data/datasources/firebase_remote_datasource.dart';
 import 'package:foodies/features/signup/domain/repositories/firebase_repository.dart';
 
 class FirebaseRepositoryImpl implements FirebaseRepository {
+  final FirebaseRemoteDataSource firebaseRemoteDataSource;
+
+  FirebaseRepositoryImpl({required this.firebaseRemoteDataSource});
+
   @override
-  Future<void> signUp(String email, String password) {
-    // TODO: implement signUp
-    throw UnimplementedError();
-  }
-  
+  Future<void> signUp(String email, String password) async =>
+      await firebaseRemoteDataSource.signUp(email, password);
+
   @override
-  Future<String> getCurrentUid() {
-    // TODO: implement getCurrentUid
-    throw UnimplementedError();
-  }
-  
+  Future<String> getCurrentUid() async =>
+      await firebaseRemoteDataSource.getCurrentUid();
+
   @override
-  Future<bool> isLogin() {
-    // TODO: implement isLogin
-    throw UnimplementedError();
-  }
-  
+  Future<bool> isLogin() async => await firebaseRemoteDataSource.isLogin();
+
   @override
-  Future<void> login(String email, String password) {
-    // TODO: implement login
-    throw UnimplementedError();
-  }
-  
+  Future<void> login(String email, String password) async =>
+      await firebaseRemoteDataSource.login(email, password);
+
   @override
-  Future<void> createCurrentUser(String email, String firstName, String lastName, String uid, String userClass, String hall, String floor, String wing, String roomNo) {
-    // TODO: implement createCurrentUser
-    throw UnimplementedError();
+  Future<void> createCurrentUser(
+      String email,
+      String firstName,
+      String lastName,
+      String uid,
+      String userClass,
+      String hall,
+      String floor,
+      String wing,
+      String roomNo) async {
+    return await firebaseRemoteDataSource.createCurrentUser(
+      email,
+      firstName,
+      lastName,
+      uid,
+      userClass,
+      hall,
+      floor,
+      wing,
+      roomNo,
+    );
   }
 }

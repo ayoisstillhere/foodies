d046c4e0c3c7559981ce7413083cdab0e91192bd
Ayodele Fagbami <afagbami@gmail.com>
2022-08-04 13:43:50 +0100

    added form constants

--- lib/constants.dart
+++ lib/constants.dart
@@ -4,3 +4,13 @@ const kPrimaryColor = Color(0xFF191919);
 const kSecondaryColor = Color(0xFFF15025);
 const kTextGreyColor = Color(0xFFCED0CE);
 const kObjectGreyColor = Color(0xFFE6E8E6);
+
+//Form Constants
+final RegExp emailValidatorRegExp =
+    RegExp(r"^[a-zA-Z0-9.]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
+const String kEmailNullError = "Please Enter your email";
+const String kInvalidEmailError = "Please Enter Valid Email";
+const String kPassNullError = "Please Enter your password";
+const String kShortPassError = "Password is too short";
+const String kMatchPassError = "Passwords don't match";
+const String kNameNullError = "Please Enter your name";

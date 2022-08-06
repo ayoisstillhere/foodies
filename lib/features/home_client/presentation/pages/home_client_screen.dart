98d6cf7e52ac337eb8557bd03de90db90dfb4a08
Ayodele Fagbami <afagbami@gmail.com>
2022-08-07 00:38:35 +0100

    Created Home Header

--- lib/features/home_client/presentation/pages/home_client_screen.dart
+++ lib/features/home_client/presentation/pages/home_client_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:foodies/components/default_button.dart';
@@ -16,17 +17,50 @@ class HomeClientScreen extends StatelessWidget {
     FirebaseAuth auth = FirebaseAuth.instance;
     SizeConfig().init(context);
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Home"),
-      ),
-      body: Center(
-        child: DefaultButton(
-          text: "SignOut",
-          press: () {
-            auth.signOut();
-            Navigator.pushNamed(context, OnboardingScreen.routeName);
-          },
-          color: kSecondaryColor,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              width: SizeConfig.screenWidth,
+              height: getProportionateScreenHeight(186),
+              decoration: const BoxDecoration(
+                color: kPrimaryColor,
+              ),
+              child: Padding(
+                padding: EdgeInsets.symmetric(
+                    horizontal: getProportionateScreenWidth(31)),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(height: getProportionateScreenHeight(54)),
+                    SizedBox(
+                      height: getProportionateScreenHeight(51.44),
+                      width: getProportionateScreenWidth(220),
+                      child: Image.asset("assets/images/Logo_Name.png"),
+                    ),
+                    SizedBox(height: getProportionateScreenHeight(20)),
+                    Text(
+                      "Welcome, Ayodele!",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: getProportionateScreenWidth(24)),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Center(
+              child: DefaultButton(
+                text: "SignOut",
+                press: () {
+                  auth.signOut();
+                  Navigator.pushNamed(context, OnboardingScreen.routeName);
+                },
+                color: kSecondaryColor,
+              ),
+            ),
+          ],
         ),
       ),
     );

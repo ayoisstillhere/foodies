5058543713a4fe1815d65b992cb51cdead17906b
Ayodele Fagbami <afagbami@gmail.com>
2022-08-07 11:28:36 +0100

    user bloc config

--- lib/features/home_client/presentation/user_bloc/user_cubit.dart
+++ lib/features/home_client/presentation/user_bloc/user_cubit.dart
@@ -1,8 +1,25 @@
+import 'dart:io';
+
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:foodies/features/signup/domain/entities/user_entity.dart';
+
+import 'package:foodies/features/signup/domain/usecases/get_users_usecase.dart';
 
 part 'user_state.dart';
 
 class UserCubit extends Cubit<UserState> {
-  UserCubit() : super(UserInitial());
+  final GetUsersUsecase usersUsecase;
+  UserCubit(
+    this.usersUsecase,
+  ) : super(UserInitial());
+
+  Future<void> getUsers() async {
+    try {
+      final user = usersUsecase.call();
+      user.listen((users) {
+        emit(UserLoaded(users: users));
+      });
+    } on SocketException catch (_) {}
+  }
 }
--- lib/features/home_client/presentation/user_bloc/user_state.dart
+++ lib/features/home_client/presentation/user_bloc/user_state.dart
@@ -2,9 +2,24 @@ part of 'user_cubit.dart';
 
 abstract class UserState extends Equatable {
   const UserState();
+}
 
+class UserInitial extends UserState {
   @override
   List<Object> get props => [];
 }
 
-class UserInitial extends UserState {}
+class UserLoaded extends UserState {
+  final List<UserEntity> users;
+  UserLoaded({
+    required this.users,
+  });
+  @override
+  List<Object?> get props => [users];
+}
+
+class UserLoading extends UserState {
+  @override
+  // TODO: implement props
+  List<Object?> get props => throw UnimplementedError();
+}

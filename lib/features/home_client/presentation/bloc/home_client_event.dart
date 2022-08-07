996d1869d24799d5f1339c0d0d0417b83f72fa42
Ayodele Fagbami <afagbami@gmail.com>
2022-08-07 10:12:20 +0100

    Added user bloc for state mgmt

--- lib/features/home_client/presentation/bloc/home_client_event.dart
+++
@@ -1,8 +0,0 @@
-part of 'home_client_bloc.dart';
-
-abstract class HomeClientEvent extends Equatable {
-  const HomeClientEvent();
-
-  @override
-  List<Object> get props => [];
-}
--- lib/features/home_client/presentation/bloc/home_client_state.dart
+++
@@ -1,9 +0,0 @@
-part of 'home_client_bloc.dart';
-
-abstract class HomeClientState extends Equatable {
-  const HomeClientState();  
-
-  @override
-  List<Object> get props => [];
-}
-class HomeClientInitial extends HomeClientState {}

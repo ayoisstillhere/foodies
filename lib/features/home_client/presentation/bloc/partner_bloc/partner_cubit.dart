f1f82d89829a96bf482f0353e849669a3660c94e
Ayodele Fagbami <afagbami@gmail.com>
2022-08-09 10:16:20 +0100

    deleted partner bloc

--- lib/features/home_client/presentation/bloc/partner_bloc/partner_cubit.dart
+++
@@ -1,8 +0,0 @@
-import 'package:bloc/bloc.dart';
-import 'package:equatable/equatable.dart';
-
-part 'partner_state.dart';
-
-class PartnerCubit extends Cubit<PartnerState> {
-  PartnerCubit() : super(PartnerInitial());
-}

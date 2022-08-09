423b88071cb461721057e8b0705f1df5451d41c4
Ayodele Fagbami <afagbami@gmail.com>
2022-08-09 15:01:48 +0100

    deleted home partner folder

--- lib/features/home_partner/presentation/bloc/home_partner_event.dart
+++
@@ -1,8 +0,0 @@
-part of 'home_partner_bloc.dart';
-
-abstract class HomePartnerEvent extends Equatable {
-  const HomePartnerEvent();
-
-  @override
-  List<Object> get props => [];
-}
--- lib/features/home_partner/presentation/bloc/home_partner_state.dart
+++
@@ -1,9 +0,0 @@
-part of 'home_partner_bloc.dart';
-
-abstract class HomePartnerState extends Equatable {
-  const HomePartnerState();  
-
-  @override
-  List<Object> get props => [];
-}
-class HomePartnerInitial extends HomePartnerState {}

8c0580b1a5c3e1710a0cd8cede7201fe39a30ed4
Ayodele Fagbami <afagbami@gmail.com>
2022-08-10 11:13:49 +0100

    created add order

---
+++ lib/features/add_order/presentation/pages/add_order.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AddOrder extends StatelessWidget {
+  const AddOrder({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Add Order"),
+      ),
+    );
+  }
+}

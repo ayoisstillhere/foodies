afb9bf613530b2c1a34323114e5b446649470827
Ayodele Fagbami <afagbami@gmail.com>
2022-08-10 12:28:15 +0100

    created order entity

---
+++ lib/features/add_order/domain/entities/order_entity.dart
@@ -0,0 +1,18 @@
+import 'package:equatable/equatable.dart';
+
+class OrderEntity extends Equatable {
+  final String uid;
+  final String name;
+  final String room;
+  final String food;
+  final String location;
+  final String amount;
+  final String details;
+  final String status;
+  final String partnerAssigned;
+
+  const OrderEntity(this.uid, this.name, this.room, this.food, this.location, this.amount, this.details, this.status, this.partnerAssigned);
+
+  @override
+  List<Object?> get props => [uid, name, room, food, location, amount, details, status, partnerAssigned];
+}

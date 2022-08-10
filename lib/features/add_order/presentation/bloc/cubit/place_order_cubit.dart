10ba51bdca2f235e1905cc81f3e3db31660c0108
Ayodele Fagbami <afagbami@gmail.com>
2022-08-10 14:44:49 +0100

    setup state mgmt for place order

--- lib/features/add_order/presentation/bloc/cubit/place_order_cubit.dart
+++ lib/features/add_order/presentation/bloc/cubit/place_order_cubit.dart
@@ -1,8 +1,48 @@
+import 'dart:io';
+
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+
+import 'package:foodies/features/add_order/domain/usecases/place_order_usecase.dart';
 
 part 'place_order_state.dart';
 
 class PlaceOrderCubit extends Cubit<PlaceOrderState> {
-  PlaceOrderCubit() : super(PlaceOrderInitial());
+  final PlaceOrderUseCase placeOrderUseCase;
+
+  PlaceOrderCubit(
+    this.placeOrderUseCase,
+  ) : super(PlaceOrderInitial());
+
+  Future<void> placeOrder({
+    required String uid,
+    required String name,
+    required String room,
+    required String food,
+    required String location,
+    required String amount,
+    required String details,
+    required String status,
+    required String partnerAssigned,
+  }) async {
+    emit(PlaceOrderLoading());
+    try {
+      await placeOrderUseCase.call(
+        uid,
+        name,
+        room,
+        food,
+        location,
+        amount,
+        details,
+        status,
+        partnerAssigned,
+      );
+      emit(PlaceOrderSuccess());
+    } on SocketException catch (e) {
+      emit(PlaceOrderFailure(e.message));
+    } catch (_) {
+      emit(PlaceOrderFailure("Firebase Exception"));
+    }
+  }
 }
--- lib/features/add_order/presentation/bloc/cubit/place_order_state.dart
+++ lib/features/add_order/presentation/bloc/cubit/place_order_state.dart
@@ -2,9 +2,29 @@ part of 'place_order_cubit.dart';
 
 abstract class PlaceOrderState extends Equatable {
   const PlaceOrderState();
+}
 
+class PlaceOrderInitial extends PlaceOrderState {
   @override
   List<Object> get props => [];
 }
 
-class PlaceOrderInitial extends PlaceOrderState {}
+class PlaceOrderLoading extends PlaceOrderState {
+  @override
+  List<Object?> get props => [];
+}
+
+class PlaceOrderFailure extends PlaceOrderState {
+  final String errorMessage;
+
+  PlaceOrderFailure(this.errorMessage);
+
+
+  @override
+  List<Object?> get props => [this.errorMessage];
+}
+
+class PlaceOrderSuccess extends PlaceOrderState{
+  @override
+  List<Object?> get props => [];
+}

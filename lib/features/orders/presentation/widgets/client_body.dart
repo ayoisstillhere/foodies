99702db2dc7a0bce046696fd58a595eda19f48f0
Ayodele Fagbami <afagbami@gmail.com>
2022-08-11 14:36:45 +0100

    completed food tile design

--- lib/features/orders/presentation/widgets/client_body.dart
+++ lib/features/orders/presentation/widgets/client_body.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:foodies/constants.dart';
 
 import '../../../../components/form_header.dart';
+import '../../../../size_config.dart';
 
 class ClientBody extends StatelessWidget {
   const ClientBody({
@@ -15,13 +18,78 @@ class ClientBody extends StatelessWidget {
       child: Center(
         child: Column(
           children: [
-            FormHeader(
+            const FormHeader(
               title: "Your Orders",
               subTitle: "Tap Orders for Details",
             ),
+            FoodTile(),
           ],
         ),
       ),
+    );
+  }
+}
+
+class FoodTile extends StatelessWidget {
+  const FoodTile({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Container(
+          height: getProportionateScreenHeight(100),
+          width: getProportionateScreenWidth(328),
+          decoration: BoxDecoration(
+            color: kObjectGreyColor,
+            borderRadius: BorderRadius.all(
+                Radius.circular(getProportionateScreenWidth(20))),
+          ),
+        ),
+        Positioned(
+          top: getProportionateScreenHeight(10),
+          left: getProportionateScreenWidth(10),
+          child: CircleAvatar(
+            radius: getProportionateScreenHeight(40),
+            backgroundColor: Colors.white,
+          ),
+        ),
+        Positioned(
+          top: getProportionateScreenHeight(12),
+          left: getProportionateScreenWidth(17),
+          child: SvgPicture.asset("assets/images/food_icon1.svg"),
+        ),
+        Positioned(
+          top: getProportionateScreenHeight(28),
+          left: getProportionateScreenWidth(105),
+          child: RichText(
+            text: TextSpan(
+              text: "Chicken and Chips\n",
+              style: TextStyle(
+                color: kPrimaryColor,
+                fontSize: getProportionateScreenWidth(18),
+              ),
+              children: <TextSpan>[
+                TextSpan(
+                    text: "CAFE 2  ",
+                    style: TextStyle(
+                      color: kPrimaryColor,
+                      fontSize: getProportionateScreenWidth(14),
+                    )),
+                TextSpan(
+                  text: "#1200",
+                  style: TextStyle(
+                    color: kSecondaryColor,
+                    fontSize: getProportionateScreenWidth(14),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
     );
   }
 }

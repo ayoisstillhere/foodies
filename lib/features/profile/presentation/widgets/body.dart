0d0e16effc56295538f3ca5c52ebb5f43ee08c31
Ayodele Fagbami <afagbami@gmail.com>
2022-08-15 13:41:11 +0100

    retrieved user from firestore

--- lib/features/profile/presentation/widgets/body.dart
+++ lib/features/profile/presentation/widgets/body.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:foodies/components/form_header.dart';
 import 'package:foodies/constants.dart';
+import 'package:foodies/features/home/presentation/bloc/user_bloc/user_cubit.dart';
 import 'package:foodies/size_config.dart';
+
+import '../../../signup/data/models/user_model.dart';
 
 class Body extends StatefulWidget {
   const Body({
@@ -16,7 +20,37 @@ class Body extends StatefulWidget {
 
 class _BodyState extends State<Body> {
   @override
+  void initState() {
+    BlocProvider.of<UserCubit>(context).getUsers();
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    return BlocBuilder<UserCubit, UserState>(
+      builder: (_, state) {
+        if (state is UserLoaded) {
+          return profileBody(state);
+        }
+        return const Center(child: CircularProgressIndicator());
+      },
+    );
+  }
+
+  Center profileBody(UserLoaded users) {
+    final user = users.users.firstWhere(
+      (user) => user.uid == widget.uid,
+      orElse: () => const UserModel(
+          email: "",
+          firstName: "",
+          lastName: "",
+          uid: "",
+          userClass: "",
+          hall: "",
+          floor: "",
+          wing: "",
+          roomNo: ""),
+    );
     return Center(
       child: Column(
         children: [
@@ -25,14 +59,14 @@ class _BodyState extends State<Body> {
             subTitle: "You can switch to be a partner from here.",
           ),
           Text(
-            "Ayodele Fagbami",
+            "${user.firstName} ${user.lastName}",
             style: TextStyle(
               color: kPrimaryColor,
               fontSize: getProportionateScreenWidth(25),
             ),
           ),
           Text(
-            "H304",
+            user.roomNo,
             style: TextStyle(
               color: kSecondaryColor,
               fontSize: getProportionateScreenWidth(18),

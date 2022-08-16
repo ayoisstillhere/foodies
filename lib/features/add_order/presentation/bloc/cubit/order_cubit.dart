f425c943c756c1c1bd242e0f1751a5b7dd5940eb
Ayodele Fagbami <afagbami@gmail.com>
2022-08-16 13:10:02 +0100

    added details methods to orderCubit

--- lib/features/add_order/presentation/bloc/cubit/order_cubit.dart
+++ lib/features/add_order/presentation/bloc/cubit/order_cubit.dart
@@ -2,8 +2,11 @@ import 'dart:io';
 
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
-import 'package:foodies/features/add_order/domain/entities/order_entity.dart';
 
+import 'package:foodies/features/add_order/domain/entities/order_entity.dart';
+import 'package:foodies/features/details/domain/usecases/delete_order_usecase.dart';
+import 'package:foodies/features/details/domain/usecases/select_order_usecase.dart';
+import 'package:foodies/features/details/domain/usecases/unselect_order_usecase.dart';
 import 'package:foodies/features/orders/domain/usecases/get_orders_usecase.dart';
 
 import '../../../domain/usecases/place_order_usecase.dart';
@@ -13,10 +16,16 @@ part 'order_state.dart';
 class OrderCubit extends Cubit<OrderState> {
   final PlaceOrderUseCase placeOrderUseCase;
   final GetOrdersUseCase getOrdersUseCase;
+  final DeleteOrderUseCae deleteOrderUseCae;
+  final SelectOrderUseCase selectOrderUseCase;
+  final UnselectOrderUseCase unselectOrderUseCase;
 
   OrderCubit({
     required this.placeOrderUseCase,
     required this.getOrdersUseCase,
+    required this.deleteOrderUseCae,
+    required this.selectOrderUseCase,
+    required this.unselectOrderUseCase,
   }) : super(OrderInitial());
 
   Future<void> placeOrder({
@@ -57,6 +66,24 @@ class OrderCubit extends Cubit<OrderState> {
       order.listen((orders) {
         emit(OrderLoaded(orders: orders));
       });
+    } on SocketException catch (_) {}
+  }
+
+  Future<void> deleteOrer(String orderId) async {
+    try {
+      await deleteOrderUseCae.call(orderId);
+    } on SocketException catch (_) {}
+  }
+
+  Future<void> selectOrder(String orderId, String partnerId) async {
+    try {
+      await selectOrderUseCase.call(orderId, partnerId);
+    } on SocketException catch (_) {}
+  }
+
+  Future<void> unselectOrder(String orderId) async {
+    try {
+      await unselectOrderUseCase.call(orderId);
     } on SocketException catch (_) {}
   }
 }

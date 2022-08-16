c12ed34cc09b7500a64074df9b217246bd440a53
Ayodele Fagbami <afagbami@gmail.com>
2022-08-16 09:50:35 +0100

    created sign out usecase

---
+++ lib/features/profile/domain/usecases/sign_out_usecase.dart
@@ -0,0 +1,12 @@
+import 'package:foodies/features/signup/domain/repositories/firebase_repository.dart';
+
+class SignOutUseCase {
+  final FirebaseRepository repository;
+  SignOutUseCase({
+    required this.repository,
+  });
+
+  Future<void> call() async {
+    return await repository.signOut();
+  }
+}
